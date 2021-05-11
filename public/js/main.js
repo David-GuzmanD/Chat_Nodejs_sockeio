bd71d170a69a625f0f89c04e2e34bd121162492f
David Guzman <davidt.guzman10@gmail.com>
2021-05-11 10:11:05 -0500

    Mensajes del chat en el DOM con Socket.io

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,30 @@
+const chatForm = document.getElementById('chat-form');
 const socket = io();
 
 socket.on('message', message => {
 
-    console.log(message);
+    // console.log(message);
+    outputMessage(message);
 
-})
+});
+
+chatForm.addEventListener('submit', (e)=> {
+    e.preventDefault();
+    
+    //Obteniendo el valor o texto del input
+    const msg = e.target.elements.msg.value;
+
+    socket.emit('chatMessage', msg);
+
+    // console.log(msg);
+});
+
+function outputMessage(msg){
+    const div = document.createElement('div');
+
+    div.classList.add('message');
+    div.innerHTML = `<p class = "meta"> David <span>9:12pm </span></p>
+    <p class = "text"> ${msg} </p> `;
+
+    document.querySelector('.chat-messages').appendChild(div);
+};
--- server.js
+++ server.js
@@ -12,7 +12,20 @@ app.use(express.static(path.join(__dirname, 'public')));
 io.on('connection', socket => {
 
     console.log('New Ws Connection...');
+    //Mensaje solo al usuario que se conecta
     socket.emit('message', 'Welcome to chat');
+
+    // Mensaje a todos los clientes o usuario, excepto el que se esta conectando al instante
+    socket.broadcast.emit('message', 'Un usuario se ha conectado al chat');
+
+    //Mensaje general
+    io.emit('message', 'Welcome to chat');
+
+    socket.on('chatMessage', msg => {
+        console.log(msg);
+        //mensaje a tdos los usuarios conectados en el socket
+        io.emit('message', msg);
+    });
 
 });
 
